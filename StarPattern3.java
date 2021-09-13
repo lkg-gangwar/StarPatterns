ba08305537202c2743181a8cb01bcc772618dcc9
Lalit Kumar Gangwar <gangwarlalitk@gmail.com>
2021-09-13 20:02:29 +0530

    COMMIT

---
+++ StarPattern3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class StarPattern2
+{
+    public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
+       int n = s.nextInt();
+       
+       for(int i=0; i<n; i++)
+       {
+           for(int j=5; j>0; j--)
+           {
+               System.out.print("*");
+           } 
+           System.out.println();
+       } 
+       s.close();
+    }
+}
