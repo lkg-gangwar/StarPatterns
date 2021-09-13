64a8ea27521e9aca68ed4c89b71dd5e1ef6ebc29
Lalit Kumar Gangwar <gangwarlalitk@gmail.com>
2021-09-13 20:34:34 +0530

    commit

--- StarPattern3.java
+++ StarPattern3.java
@@ -1,15 +1,15 @@
 import java.util.*;
-class StarPattern2
+class StarPattern
 {
     public static void main(String[] args) {
        Scanner s = new Scanner(System.in);
        int n = s.nextInt();
        
-       for(int i=0; i<n; i++)
+       for(int i=1; i<=n; i++)
        {
-           for(int j=5; j>0; j--)
+           for(int j=1; j<=n-i+1; j++)
            {
-               System.out.print("* ");
+               System.out.print("*");
            } 
            System.out.println();
        } 
--- StarPattern5.java
+++ StarPattern5.java
@@ -1,16 +1,22 @@
 import java.util.*;
-class StarPattern2
+class StarPattern5
 {
     public static void main(String[] args) {
        Scanner s = new Scanner(System.in);
        int n = s.nextInt();
        
-       for(int i=0; i<n; i++)
+       for(int i=1; i<=n; i++)
        {
-           for(int j=0; j<=i; j++)
+           for(int j=1; j<=i; j++)
            {
                System.out.print("*");
-           } 
+           }
+           System.out.println();
+
+           for(int j=n; j>=1; j--)
+           {
+               System.out.print("*");
+           }
            System.out.println();
        } 
        s.close();
